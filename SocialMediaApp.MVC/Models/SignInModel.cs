44b22b5a42cac0442d9a30feb10b17f70379074e
Konstantinos Kinnas <kinnaskonstantinos0@gmail.com>
2024-02-12 12:42:00 +0200

    Added Login Feature - Closes Issue 8
    Implemented the login feature for the app adding the necessary actions in the controller account and the necessary views and also the SignIn model. Also added persistent login.

---
+++ SocialMediaApp.MVC/Models/SignInModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialMediaApp.MVC.Models;
+
+public class SignInModel
+{
+    [Required(ErrorMessage = "This field is required")]
+    public string? Username { get; set; }
+    [Required(ErrorMessage = "This field is required")]
+    public string? Password { get; set; }
+    public bool RememberMe { get; set; }
+}
